01d25dca7734de559741fe20e7130d2de1d63ae4
Nethra <Nethra@DESKTOP-P78I51F>
2022-07-06 17:16:57 +0530

    first commit

---
+++ loops/src/forloops/Patternprogram.java
@@ -0,0 +1,12 @@
+package forloops;
+
+public class Patternprogram {
+
+	public static void main(String[] args) {
+		for(int rows=1; rows<=5; rows--) {
+			for(int columns=1; columns<=rows; columns--) {
+				System.out.print("* ");
+	}
+System.out.println(); //outer for loop
+}
+}}
