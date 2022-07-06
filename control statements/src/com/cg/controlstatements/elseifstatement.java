de957bd8cfdffc6800d68100bc6b7cba94cd31f5
Nethra <Nethra@DESKTOP-P78I51F>
2022-07-06 15:49:16 +0530

    first commit

---
+++ control statements/src/com/cg/controlstatements/elseifstatement.java
@@ -0,0 +1,20 @@
+package com.cg.controlstatements;
+
+public class elseifstatement {
+
+	public static void main(String[] args) {
+		String lang="Java";
+		if (lang=="python") {
+			System.out.println("lang is python");
+		}else if(lang=="c++") {
+			System.out.println("lang is c++");
+			}else if(lang=="Java") {
+				System.out.println("lang is Java");
+			}else
+			{
+				System.out.println("lang");
+			}
+
+	}
+
+}
